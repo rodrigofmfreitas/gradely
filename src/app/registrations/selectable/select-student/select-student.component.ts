11fc3930e5f8c3c7ee5df35ff1b8f5c823232e17
Rodrigo Melo <digofmf@gmail.com>
2024-11-25 13:25:01 -0300

    s

--- src/app/registrations/selectable/select-student/select-student.component.ts
+++ src/app/registrations/selectable/select-student/select-student.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-select-student',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './select-student.component.html',
   styleUrl: './select-student.component.css'
 })
